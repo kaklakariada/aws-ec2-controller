870360d3201e36307a1202dc3cbb701cc665933b
kaklakariada <kaklakariada@users.noreply.github.com>
2020-12-19 14:19:24 +0100

    Suppress warnings

--- infrastructure/jest.config.js
+++ infrastructure/jest.config.js
@@ -1,9 +1,10 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    "roots": [
-      "<rootDir>/test"
-    ],
-    testMatch: [ '**/*.test.ts'],
-    "transform": {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-  }
+  "roots": [
+    "<rootDir>/test"
+  ],
+  testMatch: ['**/*.test.ts'],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+}
